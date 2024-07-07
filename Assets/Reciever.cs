11d5d60948436cb8ac24ee9ea7282322d6f8f1c0
LQY <1138576280@qq.com>
2024-07-07 13:35:30 +0800

    完成Reciver的逻辑

---
+++ Assets/Reciever.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reciever : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector2 NextCameraPoisition;
+    private bool IsActived = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CameraMove()
+    {
+        IsActived = true;
+        Debug.Log("Camera Move");
+        if(IsActived) Camera.main.transform.position = NextCameraPoisition;
+    }
+}
